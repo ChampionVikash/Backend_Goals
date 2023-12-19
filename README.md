bbfdc9528a74361da3579ea00de166ba92e32bea
ChampionVikash <4355vikashkumar4355@gmail.com>
2023-12-19 13:59:49 +0530

    Goals controller and routes setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Backend Project Overview
+
+This robust backend project is built on Node.js for runtime efficiency. Leveraging Express.js in tandem with Node.js enhances the development process, while MongoDB serves as the reliable database solution. Explore the powerful capabilities and seamless integration of technologies in this project.
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("Hello Vikash Your Server is Running")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5005
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+app.use('/api/goals' , require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
