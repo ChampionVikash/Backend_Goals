58d735f0d1855af0aeb70cb2da7f41c93cf2189e
ChampionVikash <4355vikashkumar4355@gmail.com>
2023-12-19 16:48:34 +0530

    Initial REST API for goals

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Backend Project Overview
 
-This robust backend project is built on Node.js for runtime efficiency. Leveraging Express.js in tandem with Node.js enhances the development process, while MongoDB serves as the reliable database solution. Explore the powerful capabilities and seamless integration of technologies in this project.
+
+This robust backend project, which features CRUD functionality, is built on Node.js for optimal runtime efficiency. Leveraging Express.js in tandem with Node.js enhances the development process, while MongoDB serves as the reliable database solution. Explore the powerful capabilities and seamless integration of technologies in this project.
--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,12 +1,15 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
 
 // @desc    Get goals
 // @route   Get /api/goals
 // @access Private
 
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get goals'})
+    const goals = await Goal.find()
+
+    res.status(200).json(goals)
 })
 
 // @desc    Set goals
@@ -18,24 +21,48 @@ const setGoal = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please add a text field')
     }
-    res.status(200).json({message: 'Set goal'})
+
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(goal)
 })
+
 
 // @desc    Update goals
 // @route   PUT /api/goals
 // @access Private
 
-const updateGoal = async (req, res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`})
-}
+const updateGoal = asyncHandler(async (req, res) => {
+   const goal = await Goal.findById(req.params.id)
+
+   if(!goal){
+    res.status(400)
+    throw new Error('Goal not found')
+   }
+
+   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new :true,
+   })
+    res.status(200).json(updatedGoal)
+})
 
 // @desc    Delete goal
 // @route   Delete /api/goals/:id
 // @access Private
 
-const deleteGoal = async (req, res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
-}
+  const deleteGoal = asyncHandler(async (req, res) => {
+    const  goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    await goal.deleteOne();
+
+    res.status(200).json({ id : req.params.id})
+})
 
 module.exports = {
     getGoals,
